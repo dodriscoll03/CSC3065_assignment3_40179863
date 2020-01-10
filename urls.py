c6e1893025fcc2fa7835fec69ad9155405952a3b
Dylan.ODriscoll <Dylan.ODriscoll@imanage.com>
2020-01-10 23:14:30 +0000

    send straight to master

--- urls.py
+++ urls.py
@@ -3,5 +3,6 @@ class Urls:
         'http://quotes.toscrape.com/page/1/',
         'http://quotes.toscrape.com/page/2/',
         'https://www.google.com/',
-        'https://en.wikipedia.org/wiki/Loan_(sports)'
+        'https://en.wikipedia.org/wiki/Loan_(sports)',
+        'https://en.wikipedia.org/wiki/Wikipedia:Very_short_featured_articles'
     ]
